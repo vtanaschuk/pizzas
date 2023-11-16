68c23f0b9eccd537e85e3dd819a0235a92b0802e
vtanashchuk <vtanashchuk@immersetech.co>
2023-11-16 11:25:07 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,134 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+const App = () => {
+
+
+  return (
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+const Header = () =>{
+  return(
+    <header className='header'>
+      <h1>FAST REACT PIZZA CO.</h1>
+    </header>
+  )
+}
+
+const Menu = () =>{
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const pizzasLength = pizzas.length;
+
+
+  return(
+    <main className='menu'>
+      <h2>OUR MENU</h2>
+
+      { pizzasLength > 0 ?
+        (
+          <ul className='pizzas'>
+            {
+              pizzas.map(el =>{
+                  return (<Pizza data={el} key={el.name}/>)
+              })
+            }
+          </ul>
+        ) : 'Sorry we work on our menu'
+      }
+
+    </main>
+  )  
+}
+
+const Pizza = ({data}) => {
+  
+  return (
+    <li className='pizza'>
+      <img src={data.photoName} alt={data.name}/>
+      <h3>{data.name}</h3>
+      <p>{data.ingredients}</p>
+      <p>{data.price}</p>
+    </li>
+  )
+  
+}
+
+
+const Footer = () =>{
+  const hours = new Date().getHours();
+  const openHours = 12;
+  const closeHours = 20;
+  const isOpen = hours >= openHours && hours <= closeHours;
+ 
+  return (
+    <footer>
+      {isOpen ? (
+          <div className='order'> 
+            <p>We're open from {openHours}:00 to {closeHours}:00. Come visit us or order online.</p> 
+            <button className='btn'>Order</button>
+          </div>
+      ) : <p>We're heppy to welcome between {openHours}:00 {closeHours}:00</p>
+      }
+    </footer>
+      
+    
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +136,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
